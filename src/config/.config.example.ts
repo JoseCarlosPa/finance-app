9f8a2b7c3e52717eb8ae9ec2ea0cafa73051d75c
Jose Carlos <josecarlospas1@gmail.com>
2022-07-09 22:06:04 -0500

    update(gitigonre): change git ignore

---
+++ src/config/.config.example.ts
@@ -0,0 +1,11 @@
+export const config  = {
+  firebaseConfig: {
+    apiKey: "your-api-key",
+    authDomain: "your-domain",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-messagner-id",
+    appId: "your-app-id",
+    measurementId: "your-messuraer-id"
+  }
+}

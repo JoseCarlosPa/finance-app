d90b2d3ef11fb308db341ba0d314dd5d823bce22
Jose Carlos <josecarlospas1@gmail.com>
2022-07-15 12:34:46 -0500

    feat(login): add new variables

--- src/pages/CreditCards.tsx
+++ src/pages/CreditCards.tsx
@@ -1,10 +1,9 @@
 import React, {useCallback, useEffect, useState} from 'react'
-import {Cash, ChartSquareBar, CreditCard, CurrencyDollar, Reply} from "heroicons-react";
+import {Cash, CreditCard, CurrencyDollar, Reply} from "heroicons-react";
 import CreditCardsModal from "../components/Modals/CreditCardsModal";
-import {collection, query, getDocs, orderBy} from "firebase/firestore";
+import {collection, query, getDocs, orderBy,doc,getDoc} from "firebase/firestore";
 import {db} from "../App";
 import {getAuth} from "firebase/auth";
-import Card from "../components/CreditCards/Card";
 import CreditCardEdit from "../components/Modals/CreditCardEdit";
 import Incomes from "../components/Cards/Incomes";
 import RenderCards from "../components/CreditCards/RenderCards";
@@ -24,6 +23,7 @@ const CreditCards = () => {
   const [cards, setCards] = useState<singleCard[]>([])
   const [openModal, setOpenModal] = useState<boolean>(false)
   const [openEditModal, setOpenEditModal] = useState<boolean>(false)
+  const [userData, setUserData] = useState<any>({})
   const [editCard, setEditCard] = useState<singleCard>({
     id: '',
     name: '',
@@ -61,9 +61,25 @@ const CreditCards = () => {
     });
   }, [auth.currentUser])
 
+  const getUserData = useCallback(async () => {
+    const user = auth.currentUser
+    if (user === null) {
+      return
+    }
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+      return
+    }
+    setUserData(docSnap.data())
+
+  },[auth.currentUser])
+
+
   useEffect(() => {
     return (() => {
-      getCreditCards()
+      getUserData().then()
+      getCreditCards().then()
 
     })
   }, [getCreditCards])
@@ -103,16 +119,20 @@ const CreditCards = () => {
           </div>
         </div>) : (
           <div className="flex flex-col">
-            <div className="flex flex-row justify-end">
+            <div className="flex flex-row justify-end mr-4">
               <button
                 className="bg-gradient-fuchsia hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 onClick={handleOnClick}>+ Agregar tarjeta <CreditCard/></button>
             </div>
-            <div className="flex grid grid-cols-6 justify-center items-center mb-4">
-              <Incomes total={availableBalance} icon={<Cash className="text-white" width="32" height="32"/>}
+            <div className="flex grid grid-cols-4 justify-center items-center mb-4 mt-4">
+              <Incomes total={availableBalance} icon={<CreditCard className="text-white" width="32" height="32"/>}
                        title={'Disponible'} subtitle={'Sobrante total'}/>
               <Incomes total={debt} icon={<CurrencyDollar className="text-white" width="32" height="32"/>}
-                       title={'Deuda'} subtitle={'Dueda total'}/>
+                       title={'Deuda'} subtitle={'Deuda total'}/>
+              <Incomes total={userData.global_income} icon={<Cash className="text-white" width="32" height="32"/>}
+                       title={'Ingresos'} subtitle={'Ingresos Globales'}/>
+              <Incomes total={userData.global_income} icon={<Cash className="text-white" width="32" height="32"/>}
+                       title={'Porcentaje'} subtitle={'% con respecto a deuda global'}/>
 
             </div>
             <div>
